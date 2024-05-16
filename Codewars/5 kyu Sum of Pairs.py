1342efd4ea14ec12b899d7e0d883e1440a4fc7a9
Ð <nightosan@gmail.com>
2024-05-16 10:59:36 +0500

    Initial commit

---
+++ Codewars/5 kyu Sum of Pairs.py
@@ -0,0 +1,21 @@
+def sum_pairs(ints, s):
+    res = {}
+    for digit in ints:
+        if digit in res:
+            return [res[digit], digit]
+        res[s-digit] = digit
+
+    return None
+
+
+
+
+
+
+
+
+print(sum_pairs([1, 4, 8, 7, 3, 15], 8), [1, 7],)
+print(sum_pairs([1, -2, 3, 0, -6, 1], -6), [0, -6],)
+print(sum_pairs([20, -13, 40], -7), None)
+print(sum_pairs([1, 2, 3, 4, 1, 0], 2), [1, 1],)
+print(sum_pairs([10, 5, 2, 3, 7, 5], 10), [3, 7])
--- Codewars/6 kyu  Break camelCase.py
+++ Codewars/6 kyu  Break camelCase.py
@@ -1,13 +1,18 @@
 def solution(s):
     res = []
-    for indx,value in enumerate(s):
+    for indx, value in enumerate(s):
+        print("Индекс:", indx)
+        print("Значение:", value)
         if value.isupper():
+            print("Значение является заглавной буквой")
             res.append(' ')
         res.append(value)
+        print("Текущий результат:", res)
+        print("---")
     return ''.join(res)
 
-print(solution("helloWorld"), "hello World",sep='\n')
+print(solution("helloWorld"), "hello World", sep='n')
 print()
-print(solution("camelCase"), "camel Case",sep='\n')
+print(solution("camelCase"), "camel Case", sep='n')
 print()
-print(solution("breakCamelCase"), "break Camel Case",sep='\n')
+print(solution("breakCamelCase"), "break Camel Case", sep='n')
