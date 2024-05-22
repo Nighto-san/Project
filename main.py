c3c930e9ff5b76a9da6dac916ca00e4f0d245e3b
Ð <nightosan@gmail.com>
2024-05-22 15:42:38 +0500

    Initial commit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-Напишете самое короткое обращение к селектору второго (выделенного) элемента, чтобы только ему можно было задать свойства.
-<p class="brown yellow blue">Элемент 1</p>
-<p class="red brown green blue">Элемент 2</p>
-<p class="yellow blue green">Элемент 3</p>
-<p class="silver red yellow">Элемент 4</p>
-<p class="brown green red yellow">Элемент 5</p>
 
-
+if player_wins > computer_wins:
+    print(f"Congratulations! You won.")
+else:
+    print("Computer won")
